29b5ba724c9a525155c33901b35a24824dbe9cbe
Kavithma Dharmathilake <117156528+Kavithma-Dharmathilake@users.noreply.github.com>
2023-08-30 11:16:36 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config(); 
+
+const express = require('express');
+
+
+//express app
+const app = express()
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+})
+
+
+//routes
+app.get('/', (req, res) => {
+
+    res.json({mssg: 'welcome to the app'})
+
+})
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+
+    console.log('listening on port', process.env.PORT);
+
+})
+
